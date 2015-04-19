2d319c6fe9a90e6aacf7e45c6297edb3738e2b7c
flybass <flybass@gmail.com>
2015-04-19 15:12:13 -0400

    removed misc commented out section

--- Q3-hours_worked/histo.py
+++ Q3-hours_worked/histo.py
@@ -30,22 +30,3 @@ plt.xlabel('Minutes Worked/Day')
 plt.ylabel('Daily Revenue')
 plt.savefig('Minutes_v_Rev.png', dpi=400)
 
-
-
-
-
-'''
-n, bins, patches = plt.hist(all_vals, 35, normed=1, stacked=1,facecolor='green', alpha=0.75)
-
-# add a 'best fit' line (assuming normality)
-y = mlab.normpdf( bins, np.mean(all_vals), np.std(all_vals))
-l = plt.plot(bins, y, 'r--', linewidth=1)
-
-plt.xlabel('P(tip(t+1)>median) | tip(t)>median)')
-plt.ylabel('Scaled Frequency')
-plt.title(r'Are Good tips Followed by Good Tips?')
-plt.axis([.1, .9,0,10])
-plt.grid(True)
-
-plt.savefig('Histogram.png', dpi=400)
-'''
